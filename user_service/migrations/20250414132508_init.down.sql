0f0c5dc0fe21cd1ab8c3767331fb5d10b24a0b94
DENFNC <andreev.gamepro@gmail.com>
2025-04-15 20:13:22 +0300

    feat: SQL migration is built

---
+++ user_service/migrations/20250414132508_init.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS wishlist_items;
+
+DROP TABLE IF EXISTS wishlists;
+
+DROP TABLE IF EXISTS payment_methods;
+
+DROP TABLE IF EXISTS shipping_addresses;
+
+DROP TABLE IF EXISTS profiles;
---
+++ user_service/migrations/20250414132508_init.up.sql
@@ -0,0 +1,49 @@
+-- 2.1. profiles
+CREATE TABLE
+    profiles (
+        profile_id SERIAL PRIMARY KEY,
+        auth_user_id INTEGER UNIQUE NOT NULL,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        phone VARCHAR(20),
+        created_at TIMESTAMP DEFAULT NOW (),
+        updated_at TIMESTAMP DEFAULT NOW ()
+    );
+
+-- 2.2. shipping_addresses
+CREATE TABLE
+    shipping_addresses (
+        address_id SERIAL PRIMARY KEY,
+        profile_id INTEGER NOT NULL REFERENCES profiles (profile_id),
+        country VARCHAR(50) NOT NULL,
+        city VARCHAR(50) NOT NULL,
+        street VARCHAR(100) NOT NULL,
+        postal_code VARCHAR(20) NOT NULL,
+        is_default BOOLEAN DEFAULT FALSE
+    );
+
+-- 2.3. payment_methods
+CREATE TABLE
+    payment_methods (
+        payment_id SERIAL PRIMARY KEY,
+        profile_id INTEGER NOT NULL REFERENCES profiles (profile_id),
+        payment_token VARCHAR(255) NOT NULL,
+        is_default BOOLEAN DEFAULT FALSE
+    );
+
+-- 2.4. wishlists
+CREATE TABLE
+    wishlists (
+        wishlist_id SERIAL PRIMARY KEY,
+        profile_id INTEGER NOT NULL UNIQUE REFERENCES profiles (profile_id),
+        created_at TIMESTAMP DEFAULT NOW ()
+    );
+
+-- 2.5. wishlist_items
+CREATE TABLE
+    wishlist_items (
+        item_id SERIAL PRIMARY KEY,
+        wishlist_id INTEGER NOT NULL REFERENCES wishlists (wishlist_id),
+        product_id INTEGER NOT NULL,
+        added_at TIMESTAMP DEFAULT NOW ()
+    );
