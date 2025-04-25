25c81a66531a9e37548ccf5e00e99af1e55626d2
DENFNC <andreev.gamepro@gmail.com>
2025-04-25 18:21:44 +0300

    feat: Category structure

---
+++ catalog_service/internal/handler/category/category.go
@@ -0,0 +1,34 @@
+package category
+
+import (
+	"context"
+
+	v1 "github.com/DENFNC/Zappy/catalog_service/proto/gen/v1"
+)
+
+type Category interface{}
+
+type serverAPI struct {
+	v1.UnimplementedCategoryServiceServer
+	svc Category
+}
+
+func New(serv Category) *serverAPI {
+	return &serverAPI{
+		svc: serv,
+	}
+}
+
+func (api *serverAPI) CreateCategory(
+	ctx context.Context,
+	req *v1.CreateCategoryRequest,
+) (*v1.CreateCategoryResponse, error) {
+	panic("implement me")
+}
+
+func (api *serverAPI) ListCategories(
+	ctx context.Context,
+	req *v1.ListCategoriesRequest,
+) (*v1.ListCategoriesResponse, error) {
+	panic("implement me")
+}
