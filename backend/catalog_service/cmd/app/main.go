ae3781d3f3aa618a0bea1b4a660471a05268b292
DENFNC <andreev.gamepro@gmail.com>
2025-05-07 13:25:38 +0300

    refact: Unloading the entry point and transferring initialization to the app layer

--- backend/catalog_service/cmd/app/main.go
+++ backend/catalog_service/cmd/app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,38 +9,26 @@ import (
 	"github.com/DENFNC/Zappy/catalog_service/internal/adapters/sql/postgres"
 	"github.com/DENFNC/Zappy/catalog_service/internal/app"
 	"github.com/DENFNC/Zappy/catalog_service/internal/pkg/logger"
-	"github.com/DENFNC/Zappy/catalog_service/internal/pkg/paginate"
 	"github.com/DENFNC/Zappy/catalog_service/internal/utils/config"
 )
 
 func main() {
 	// Для тестов использовать ./config/config_test.yaml
+
+	// TODO: Чтобы приложение работало нужно прописать переменные окружения
+	// TODO: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY и AWS_ENDPOINT_URL
+
 	cfg := config.MustLoad("./config/config.yaml")
 	logger, err := logger.New(cfg.LogType)
 	if err != nil {
 		panic(err)
 	}
 
-	dbpool, err := postgres.New(cfg.Postgres.URL)
-	if err != nil {
-		panic(err)
-	}
-
-	paginateCoder, err := paginate.NewEncryptor(
-		[]byte(cfg.PaginateSecret),
-		rand.Reader,
-	)
-	if err != nil {
-		panic(err)
-	}
+	dbpool := MustInitDatabasePool(cfg)
 
 	application, err := app.New(
-		context.TODO(),
-		logger,
-		dbpool,
-		cfg.GRPC.Port,
-		cfg.HTTP.Port,
-		paginateCoder,
+		context.TODO(), logger,
+		dbpool, cfg,
 	)
 	if err != nil {
 		panic(err)
@@ -60,6 +47,16 @@ func main() {
 		"signal", sig.String(),
 	)
 
-	dbpool.Stop()
 	application.App.Stop()
+}
+
+func MustInitDatabasePool(
+	cfg *config.Config,
+) *postgres.Storage {
+	dbpool, err := postgres.New(cfg.Postgres.URL)
+	if err != nil {
+		panic(err)
+	}
+
+	return dbpool
 }
--- backend/catalog_service/internal/app/app.go
+++ backend/catalog_service/internal/app/app.go
@@ -2,15 +2,21 @@ package app
 
 import (
 	"context"
+	"crypto/rand"
 	"log/slog"
+	"time"
 
+	s3client "github.com/DENFNC/Zappy/catalog_service/internal/adapters/aws/s3"
+	"github.com/DENFNC/Zappy/catalog_service/internal/adapters/aws/s3/store"
 	"github.com/DENFNC/Zappy/catalog_service/internal/adapters/sql/postgres"
 	"github.com/DENFNC/Zappy/catalog_service/internal/adapters/sql/postgres/repo"
 	grpcapp "github.com/DENFNC/Zappy/catalog_service/internal/app/grpc"
 	"github.com/DENFNC/Zappy/catalog_service/internal/handler/category"
 	"github.com/DENFNC/Zappy/catalog_service/internal/handler/product"
+	productimage "github.com/DENFNC/Zappy/catalog_service/internal/handler/product_image"
 	"github.com/DENFNC/Zappy/catalog_service/internal/pkg/paginate"
 	"github.com/DENFNC/Zappy/catalog_service/internal/service"
+	"github.com/DENFNC/Zappy/catalog_service/internal/utils/config"
 )
 
 type App struct {
@@ -21,16 +27,26 @@ func New(
 	ctx context.Context,
 	log *slog.Logger,
 	db *postgres.Storage,
-	grpcPort int,
-	httpPort int,
-	coder paginate.TokenCoder,
+	cfg *config.Config,
 ) (*App, error) {
+	paginateCoder, err := InitPaginateCoder(cfg)
+	if err != nil {
+		return nil, err
+	}
 
-	productRepo := repo.NewProductRepo(db, db.Dial, coder)
+	objectStore, err := InitStore()
+	if err != nil {
+		return nil, err
+	}
+
+	productRepo := repo.NewProductRepo(db, db.Dial, paginateCoder)
 	productSvc := service.NewProduct(log, productRepo)
 	productHandle := product.New(productSvc)
 
-	categoryRepo := repo.NewCategoryRepo(db, db.Dial, coder)
+	productImageSvc := service.NewProductImage(log, objectStore)
+	productImageHandle := productimage.New(productImageSvc, cfg.ObjectStore.AwsBucketImage)
+
+	categoryRepo := repo.NewCategoryRepo(db, db.Dial, paginateCoder)
 	categorySvc := service.NewCategory(log, categoryRepo)
 	categoryHandle := category.New(categorySvc)
 
@@ -38,10 +54,39 @@ func New(
 		App: *grpcapp.New(
 			ctx,
 			log,
-			grpcPort,
-			httpPort,
+			cfg.GRPC.Port,
+			cfg.HTTP.Port,
 			productHandle,
 			categoryHandle,
+			productImageHandle,
 		),
 	}, nil
+}
+
+func InitStore() (*store.Store, error) {
+	s3Store, err := s3client.NewClient(
+		context.TODO(),
+		s3client.WithPresignExpiry(time.Second*5),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	store := store.NewStore(s3Store)
+
+	return store, nil
+}
+
+func InitPaginateCoder(
+	cfg *config.Config,
+) (*paginate.Encryptor, error) {
+	paginateCoder, err := paginate.NewEncryptor(
+		[]byte(cfg.PaginateSecret),
+		rand.Reader,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return paginateCoder, nil
 }
