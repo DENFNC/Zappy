187ebd8bf8dfdd683042ff047e3a4f5e68ca5c14
DENFNC <andreev.gamepro@gmail.com>
2025-05-16 14:29:54 +0300

    chore: Adapting configurations to meet new requirements

--- backend/catalog_service/internal/utils/config/config.go
+++ backend/catalog_service/internal/utils/config/config.go
@@ -8,31 +8,41 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
-type Config struct {
-	LogType        string `yaml:"log_type" env-default:"dev"`
-	PaginateSecret string `yaml:"paginate_secret" env-required:"true"`
-	GRPC           ConfigGRPC
-	HTTP           ConfigHTTP
-	Postgres       ConfigPSQL
-	ObjectStore    ObjectConfig `yaml:"object_store"`
-}
+type (
+	Config struct {
+		LogType        string `yaml:"log_type" env-default:"dev"`
+		PaginateSecret string `yaml:"paginate_secret" env-required:"true"`
+		GRPC           ConfigGRPC
+		HTTP           ConfigHTTP
+		Postgres       ConfigPSQL
+		ObjectStore    BucketCfg `yaml:"object_store"`
+	}
 
-type ConfigGRPC struct {
-	Port    int           `yaml:"port" env-required:"true"`
-	Timeout time.Duration `yaml:"timeout"`
-}
+	ConfigGRPC struct {
+		Port       int           `yaml:"port" env-required:"true"`
+		Timeout    time.Duration `yaml:"timeout"`
+		Reflection bool          `yaml:"reflection"`
+	}
 
-type ConfigHTTP struct {
-	Port int `yaml:"port" env-required:"true"`
-}
+	ConfigHTTP struct {
+		Port int `yaml:"port" env-required:"true"`
+	}
 
-type ConfigPSQL struct {
-	URL string `yaml:"url" env-required:"true"`
-}
+	ConfigPSQL struct {
+		URL string `yaml:"url" env-required:"true"`
+	}
 
-type ObjectConfig struct {
-	AwsBucketImage string `yaml:"aws_bucket_image" end-required:"true"`
-}
+	BucketCfg struct {
+		ObjectOrigin  string `yaml:"object_origin" env-default:"http://localhost:9000"`
+		ImageBucket   string `yaml:"aws_bucket_image" env-required:"true"`
+		StagingBucket string `yaml:"aws_staging_bucket" env-required:"true"`
+		Buckets       map[string]struct {
+			Name      string   `yaml:"name"`
+			MimeTypes []string `yaml:"mime_types"`
+			Path      string   `yaml:"path"`
+		} `yaml:"buckets"`
+	}
+)
 
 func MustLoad(path string) *Config {
 	if path == "" {
